18e4a6206f4bb5eb532c91a47130510c26c0ea9b
unknown <Justin@.(none)>
2009-01-29 00:47:21 -0800

    Added code for neural netowrk that acts like an AND conditional statement.

---
+++ Exploratory/IntroToNeuralNetworksCSharp/HelloWorldNeuralNetworks/HelloWorldNeuralNetworks/Network.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorldNeuralNetworks
+{
+    public class Network
+    {
+        private List<Neuron> _Neurons;
+        private Dictionary<Neuron, List<Neuron>> _Connections;
+
+        public Network()
+        {
+            _Connections = new Dictionary<Neuron, List<Neuron>>();
+        }
+
+        public void Add(Neuron neuron)
+        {
+            _Neurons.Add(neuron);
+        }
+
+        public void Connect(Neuron fromNeuron, Neuron toNeuron)
+        {
+            if(!_Connections.ContainsKey(fromNeuron))
+            {
+                _Connections.Add(fromNeuron, new List<Neuron>());
+            }
+            
+            if(!_Connections[fromNeuron].Contains(toNeuron))
+            {
+                _Connections[fromNeuron].Add(toNeuron);
+            }
+        }
+
+        public bool Execute()
+        {
+            var inputs = new List<Neuron>();
+            var output = new OutputNeuron(0,0, 0);
+
+            foreach(var pair in _Connections)
+            {
+                // These are the first inputs that should fire.
+                if (pair.Key is InputNeuron)
+                {
+                    inputs.Add(pair.Key as InputNeuron);
+                }
+
+                foreach(var possibleOutput in pair.Value)
+                {
+                    if (possibleOutput is OutputNeuron)
+                        output = possibleOutput as OutputNeuron;
+                }
+            }
+
+            ProcessAndGetNext(inputs);
+            return output.Value >= 1;
+        }
+
+        private void ProcessAndGetNext(List<Neuron> nextLayer)
+        {
+            var processNext = new List<Neuron>();
+            foreach(var neuron in nextLayer)
+            {
+                foreach(var connection in _Connections[neuron])
+                {
+                    if(!processNext.Contains(connection)) 
+                        processNext.Add(connection);
+                    connection.ReceiveValue(neuron.Value * neuron.Weight);
+                }
+            }
+
+            if(processNext.Count > 1) ProcessAndGetNext(processNext);
+        }
+    }
+
+    public class InputNeuron : Neuron
+    {
+        public InputNeuron(double value, double weight)
+            : base(0, weight)
+        {
+            Value = value;
+        }
+    }
+
+    public class OutputNeuron : Neuron
+    {
+        public OutputNeuron(double threshold, double weight, double value) : base(threshold, weight)
+        {
+            Value = value;
+        }
+    }
+
+    public class Neuron
+    {
+        private double _CacheValue { get; set; }
+        public double Value { get; set; }
+        public double Threshold { get; set; }
+        public double Weight { get; set; }
+        public string Name { get; set; }
+
+        public EventHandler ThresholdCrossedEvent;
+
+        public Neuron(double threshold, double weight)
+        {
+            Threshold = threshold;
+            Weight = weight;
+        }
+
+        public void ReceiveValue(double value)
+        {
+            _CacheValue += value;
+
+            if (_CacheValue >= Threshold)
+            {
+                Value = _CacheValue;
+                _CacheValue = 0;
+            }
+        }
+
+        public bool IsThresholdTriggered()
+        {
+            return Value >= Threshold;
+        }
+    }
+}
---
+++ Exploratory/IntroToNeuralNetworksCSharp/HelloWorldNeuralNetworks/Tests/AndNetworkTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HelloWorldNeuralNetworks;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class AndNetworkTests
+    {
+        [Test]
+        public void Both_inputs_low()
+        {
+            var neuronA = new InputNeuron(0, 1);
+            var neuronB = new InputNeuron(0, 1);
+            var neuronC = new OutputNeuron(1.5, 1, 0);
+
+            var network = new Network();
+
+            network.Connect(neuronA, neuronC);
+            network.Connect(neuronB, neuronC);
+
+            Assert.IsFalse(network.Execute());
+        }
+
+        [Test]
+        public void Both_inputs_high()
+        {
+            var neuronA = new InputNeuron(1, 1);
+            var neuronB = new InputNeuron(1, 1);
+            var neuronC = new OutputNeuron(1.5, 1, 0);
+
+            var network = new Network();
+
+            network.Connect(neuronA, neuronC);
+            network.Connect(neuronB, neuronC);
+
+            Assert.IsTrue(network.Execute());
+        }
+
+        [Test]
+        public void Both_inputs_different()
+        {
+            var neuronA = new InputNeuron(0, 1);
+            var neuronB = new InputNeuron(1, 1);
+            var neuronC = new OutputNeuron(1.5, 1, 0);
+
+            var network = new Network();
+
+            network.Connect(neuronA, neuronC);
+            network.Connect(neuronB, neuronC);
+
+            Assert.IsFalse(network.Execute());
+        }
+    }
+}
