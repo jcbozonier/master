8ff6c1ba599f63bbc43c5e9095f60dfffe4c58d0
unknown <Justin@.(none)>
2009-02-07 20:17:24 -0800

    First check in for compete. Completed post-commit-hook that will build each sln in the bots folder. The next step is to automate pulling from the git repo after check in and populate the Bots folder with all of the new bot code.

---
+++ Compete/BotCode/Bots/Another SampleBot/Another SampleBot/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Another_SampleBot
+{
+    public class Class1
+    {
+    }
+}
---
+++ Compete/BotCode/Bots/BotSample1/BotSample1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BotSample1
+{
+    /// <summary>
+    /// Empty comment. Again.
+    /// </summary>
+    public class NewClass
+    {
+
+    }
+}
