785cb42296e99d6d3214c079b0edfdde3f1d375f
Justin Bozonier <darkxanthos@gmail.com>
2009-02-16 22:15:30 -0800

    My first twitter client...

---
+++ IronTwit/IronTwit/IronTwit/Models/TwitObj.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IronTwit.Models
+{
+    public class TwitObj
+    {
+        public string text { get; set; }
+        public TwitUser user { get; set; } 
+    }
+}
---
+++ IronTwit/IronTwit/IronTwit/Window1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using IronTwit.Models;
+using Yedda;
+using Newtonsoft.Json;
+
+namespace IronTwit
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            Loaded += new RoutedEventHandler(Window1_Loaded);
+        }
+
+        void Window1_Loaded(object sender, RoutedEventArgs e)
+        {
+            Yedda.Twitter twit = new Twitter();
+
+            var resultString = twit.GetUserTimeline("darkxanthos@gmail.com", "You Cant Have My Password!", Twitter.OutputFormatType.JSON);
+            var str = new StringReader(resultString);
+            var converter = new JsonSerializer();
+            converter.MissingMemberHandling = MissingMemberHandling.Ignore;
+            var obj = (List<TwitObj>)converter.Deserialize(str, typeof(List<TwitObj>));
+
+            TwitterListView.ItemsSource = obj;
+        }
+    }
+}
